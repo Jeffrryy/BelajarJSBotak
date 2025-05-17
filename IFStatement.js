923268a51eb970498cab66b7a28c8f7df849bde6
Jeffrryy <nickochristian642@gmail.com>
2025-05-17 15:08:20 +0700

    first commit

---
+++ IFStatement.js
@@ -0,0 +1,22 @@
+
+
+// let nilai = 45
+
+// //Nilai nya lebih kecil dari 100 dapet A+
+// //Nilai nya lebih kecil dari 80 dapet B
+// //Nilai nya lebih kecil dari 70 dapet C
+
+// if(nilai >= 100){
+//     console.log("A+")
+// } else if(nilai >= 80 ){
+//     console.log("B")
+// } else if (nilai >= 70){
+//     console.log("C")
+// } else if(nilai >= 60) {
+//     console.log("D")
+// } else {
+//  console.log("E")   
+// }
+
+
+
---
+++ variabel.js
@@ -0,0 +1,11 @@
+//constatata = gak bisa diganti valuenya
+
+const nama = "niko"
+ 
+let umur = 12
+
+
+var kelas = "rpl"
+
+
+console.log(kelas)
