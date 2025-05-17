3ddaf044be75b9615be62b773a024922ac3a7264
Jeffrryy <nickochristian642@gmail.com>
2025-05-17 16:48:37 +0700

    pertemuan 2

---
+++ Bitwise.js
@@ -0,0 +1,36 @@
+// //
+// let umur = 20;
+// let punyaKTP = true;
+// if (umur >= 18 && punyaKTP) {
+//   console.log("boleh masuk club");
+// } else {
+//   console.log("maap ya dek");
+// }
+
+// let angka =12;
+// let angkagenap = true;
+// if(angka >= 10 && angkagenap){
+//     console.log("angka ini genap")
+// } else {
+//     console.log("angka ini ganjil");
+// }
+
+// OR
+
+let Manajer = true;
+let punyaKartuAkses = false;
+
+if (Manajer || punyaKartuAkses)
+  console.log("AKSES DIBUKA");
+else {
+  console.log("AKSES DITOLAK");
+}
+
+let Varel = true;
+let nikonikoni = false;
+
+if (Varel || nikonikoni) {
+    console.log("ORANG INI PEDO");
+} else {
+    console.log("ORANG INI BUKAN PEDO");
+}
---
+++ Function.js
@@ -0,0 +1,29 @@
+// function deklarasi (){
+//     console.log("fungsi berjalan deklarasi")
+// }
+// deklarasi()
+
+function tambah (a,b){
+    console.log(a+b)
+}
+
+tambah(1,2)
+
+function rapot (nilai){
+    if(nilai >= 100){
+        console.log("A+")
+    } else if(nilai >= 80 ){
+        console.log("B")
+    } else if (nilai >= 70){
+        console.log("C")
+    } else if(nilai >= 60) {
+        console.log("D")
+    } else {
+     console.log("E")   
+    }
+
+}
+
+
+
+rapot(90)
